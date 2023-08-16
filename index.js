1c86bffd2939e84bd64638f85716a57ffa0bb29a
Nicolas Oste <nicolas@dnn-concept.be>
2023-08-16 18:15:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const spawnSync = require('child_process').spawnSync;
+
+module.exports.handler = async (event) => {
+  try {
+    // Execute the CLI tool command using npx
+    const env = 'staging'
+    const deploymentId = '12345'
+    // const command = ` trigger --record --tags=${env} --test-session-name=${deploymentId} --location=eu-central-1`;
+    const { stdout, stderr } = spawnSync('./node_modules/.bin/checkly', ['trigger', '--record', `--tags=${env}`, `--test-session-name=${deploymentId}`, '--location=eu-central-1']);
+    console.log(`CLI tool output: ${stdout}`);
+    console.error(`CLI tool error: ${stderr}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v3.0! Your function executed successfully!",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    console.error(`Error executing CLI tool: ${error}`);
+    return false;
+  };
+
+};
